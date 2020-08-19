6f0a72f22a30b6d4f74a63e22d0cac015546c9c4
SeoHyeonJeong <seohyeonjeong.dev@gmail.com>
2020-08-19 16:26:24 +0900

    01.자바 프로그래밍 시작하기 - Hello, Java 문장 출력하기(p.33)

---
+++ First/src/hello/HelloJava.java
@@ -0,0 +1,19 @@
+/*
+ Date : 2020년 8월 18일
+ Author : 서현정
+ Description : 첫 번째 자바 프로그램입니다.
+ Version : 1.8.0
+*/
+
+package hello;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		//System.out.println("Hello, Java"); 한 줄 주석 연습입니다.
+
+		//화면에 문장을 출력함
+		System.out.println("자바 프로그래밍 재미있다!");
+	}
+
+}
