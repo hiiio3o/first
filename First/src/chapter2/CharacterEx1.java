d2053c15495944950b8507a76898cbac2659f977
SeoHyeonJeong <seohyeonjeong.dev@gmail.com>
2020-09-18 15:54:39 +0900

    02.변수와 자료형 - 문자형 연습(p.53)

---
+++ First/src/chapter2/CharacterEx1.java
@@ -0,0 +1,29 @@
+package chapter2;
+
+public class CharacterEx1 {
+
+	public static void main(String[] args) {
+		char ch1 = 'A';
+		System.out.println(ch1); 		// 문자 출력
+		System.out.println((int) ch1); 	// 문자에 해당하는 정수 값(아스키 코드 값) 출력
+
+		char ch2 = 66; 					// 정수 값 대입
+		System.out.println(ch2); 		// 정수 값에 해당하는 문자 출력
+
+		int ch3 = 67;
+		System.out.println(ch3);		// 문자 정수 값 출력
+		System.out.println((char) ch3); // 정수 값에 해당하는 문자 출력
+
+		
+		char ch4 = 'Z';
+		System.out.println(ch4);
+		System.out.println((int)ch4);
+		char ch5 = 38;
+		System.out.println(ch5);
+		
+		int ch6 = 97;
+		System.out.println(ch6);
+		System.out.println((char)ch6);
+	}
+
+}
