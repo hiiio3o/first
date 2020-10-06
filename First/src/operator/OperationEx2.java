7952c0206cb8a5069f13995baa202b9709b4a878
SeoHyeonJeong <seohyeonjeong.dev@gmail.com>
2020-10-06 12:45:13 +0900

    03.자바의 여러 가지 연산자 - 증가·감소 연산자를 사용하여 값 연산하기(p.75)

--- First/src/operator/OperationEx2.java
+++ First/src/operator/OperationEx2.java
@@ -7,7 +7,7 @@ public class OperationEx2 {
 		int lastScore1 = ++gameScore; // gameScore에 1만큼 더한 값을 lastScore1에 대입
 		System.out.println(lastScore1); // 151
 
-		System.out.println(gameScore); 
+		System.out.println(gameScore);
 
 		int lastScore2 = --gameScore; // gameScore에 1만큼 뺀 값을 lastScore2에 대입
 		System.out.println(lastScore2); // 150
